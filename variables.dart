373e6f32771f61d5e8ad0af309de43421600f6b6
Akhilesh <AkhileshM@outlook.in>
2019-09-06 20:19:59 +0530

    Variables , comments, const, string interpolation

---
+++ variables.dart
@@ -0,0 +1,31 @@
+void main() {
+  print('Hai Akhilesh!');
+
+  String name = 'Akhilesh';
+  int age = 25;
+  double height = 5.9;
+  bool employed = true;
+
+  var gender = 'Male';
+
+  const dob = "01/07/1994";
+
+  print("Name: $name");
+  print('Age: $age');
+  print('Height: ${height}"');
+  print('Employed: ${employed}');
+  print('Gender: ${gender}');
+  print('Date of birth: ${dob}');
+
+  const firstName = 'Akhilesh';
+  const lastName = 'Muthusamy';
+
+  print('Fullname: ' + firstName + ' ' + lastName);
+
+  /*
+   * Print values from 1 to 10
+   * */
+  for (int i = 1; i <= 10; i++) {
+    print('i = ${i}');
+  }
+}
